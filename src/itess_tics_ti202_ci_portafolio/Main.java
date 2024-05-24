b6e985a2a3f99ccca5e0e6f17d43de0887e90256
Brendy <brandyaguilar001@gmail.com>
2024-05-24 13:32:21 -0600

    Inicio del proyecto

---
+++ src/itess_tics_ti202_ci_portafolio/Main.java
@@ -0,0 +1,17 @@
+/*
+ * ITESS-TICS
+ * Enero-Junio 2024
+ * Calculo Integral Portafolio
+ * TI-202
+ * 24/05/2024
+ * BAF
+ */
+package itess_tics_ti202_ci_portafolio;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("HELLO FX");
+    }
+    
+}
